31d8909f783ede219c57d2723e9d1cc4941c2600
Vladimir Kuleshov <Gottgunst@gmail.com>
2024-01-03 15:04:40 +0300

    feat: Time and Bot Name calling

--- src/commands/do.command.ts
+++ src/commands/do.command.ts
@@ -1,6 +1,6 @@
 import { Context, Telegraf } from 'telegraf';
 import { Command } from './command.class';
-import moment from 'moment';
+import moment from 'moment-timezone';
 import 'moment/locale/ru';
 import {
   IBotContext,
@@ -21,65 +21,23 @@ export class DoCommand extends Command {
       callback_data: 'instruct_task',
     },
   ];
+  botName = /@\bVist_ToDo_bot\b/i;
 
   constructor(bot: Telegraf<IBotContext>) {
     super(bot);
   }
 
-  _createTask(ctx: Context, task: string): void {
-    ctx.reply(task, {
-      parse_mode: 'HTML',
-      disable_notification: true,
-      allow_sending_without_reply: true,
-      reply_markup: {
-        inline_keyboard: [this._buttons],
-      },
-    });
-
-    this.bot.telegram.deleteMessage(
-      ctx.message!.chat.id,
-      ctx.message!.message_id,
-    );
-  }
-
-  _parseTask(text: string, type: TTaskType): string {
-    const parse = text.split('\n');
-    const date = moment().locale('ru').format('Do MMM, HH:mm');
-    const task = {
-      set: {
-        firstLine: `⚠  <b>${parse[0].slice(4)}</b>`,
-        other: parse.slice(1).join('\n'),
-        dateLine: `<code>[${date}]</code>`,
-        tagLine: ` #todo`,
-      },
-      instruct: {
-        firstLine: `<b>${parse[0]}</b>`,
-        other: parse.slice(1, -2).join('\n'),
-        dateLine: `<code>${parse.slice(-2, -1)}</code>`,
-        tagLine: `#todo${this._worker > 0 ? '_' : ''}${
-          this._workersArray[this._worker]
-        }`,
-      },
-      done: {
-        firstLine: `✅   <i>${parse[0].slice(3)}</i>`,
-        other: parse.slice(1, -3).join('\n'),
-        dateLine: `<code>[${date}]</code>`,
-        tagLine: `#done_${parse.at(-1)}`,
-      },
-    };
-
-    const mgs = task[type];
-    return `${mgs.firstLine}\n${mgs.other}\n${mgs.dateLine}\n${mgs.tagLine}`;
-  }
-
   handle(): void {
-    this.bot.hears(/\bVist_ToDo_bot\b/i, (ctx) => {
-      const task = this._parseTask(ctx.message.text.slice(11), 'set');
+    this.bot.hears(this.botName, (ctx) => {
+      const task = this._parseTask(
+        ctx.message.text.replace(this.botName, ''),
+        'set',
+      );
       this._createTask(ctx, task);
     });
 
     this.bot.command('do', (ctx) => {
-      const task = this._parseTask(ctx.message.text, 'set');
+      const task = this._parseTask(ctx.message.text.slice(4), 'set');
       this._createTask(ctx, task);
     });
 
@@ -118,5 +76,55 @@ export class DoCommand extends Command {
         },
       });
     });
+  }
+
+  _createTask(ctx: Context, task: string): void {
+    ctx.reply(task, {
+      parse_mode: 'HTML',
+      disable_notification: true,
+      allow_sending_without_reply: true,
+      reply_markup: {
+        inline_keyboard: [this._buttons],
+      },
+    });
+
+    this.bot.telegram.deleteMessage(
+      ctx.message!.chat.id,
+      ctx.message!.message_id,
+    );
+  }
+
+  _parseTask(text: string, type: TTaskType): string {
+    const parse = text.split('\n');
+    const date = moment
+      .tz('Europe/Moscow')
+      .locale('ru')
+      .format('Do MMM, HH:mm');
+
+    const task = {
+      set: {
+        firstLine: `⚠  <b>${parse[0]}</b>`,
+        other: parse.slice(1).join('\n'),
+        dateLine: `<code>[${date}]</code>`,
+        tagLine: ` #todo`,
+      },
+      instruct: {
+        firstLine: `<b>${parse[0]}</b>`,
+        other: parse.slice(1, -2).join('\n'),
+        dateLine: `<code>${parse.slice(-2, -1)}</code>`,
+        tagLine: `#todo${this._worker > 0 ? '_' : ''}${
+          this._workersArray[this._worker]
+        }`,
+      },
+      done: {
+        firstLine: `✅   <i>${parse[0].slice(3)}</i>`,
+        other: parse.slice(1, -3).join('\n'),
+        dateLine: `<code>[${date}]</code>`,
+        tagLine: `#done   <code>${parse.at(-1)}</code>`,
+      },
+    };
+
+    const mgs = task[type];
+    return `${mgs.firstLine}\n${mgs.other}\n${mgs.dateLine}\n${mgs.tagLine}`;
   }
 }
